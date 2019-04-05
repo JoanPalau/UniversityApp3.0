cd488ff488cd6b32fa240af47a1af9830810a5d0
Toni Pacheco <toni.pafe@gmail.com>
2019-04-05 17:46:14 +0200

    Added University and Degree to admin page.

--- univoting/admin.py
+++ univoting/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from univoting.models.degree import Degree, University
 
 # Register your models here.
+admin.site.register(University)
+admin.site.register(Degree)

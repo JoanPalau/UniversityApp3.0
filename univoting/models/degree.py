d0e4b965ea6452228c2aa1d6a73b23a8d516cfce
Toni Pacheco <toni.pafe@gmail.com>
2019-04-05 17:00:55 +0200

    Added validators to Degree model

--- univoting/models/degree.py
+++ univoting/models/degree.py
@@ -1,4 +1,6 @@
 import re
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy
 from django.db import models
 from django.core.validators import MaxLengthValidator, MinValueValidator
 
@@ -7,25 +9,34 @@ class University(models.Model):
     pass
 
 
+def validate_title(value):
+    if not re.match(r'^(\w ?)+$', value):
+        raise ValidationError(
+            gettext_lazy('%(value) no es un nom de producte.'),
+            params={'value': value}
+        )
+
+
 class Degree(models.Model):
-    title = models.CharField(max_length=64)
+    title = models.CharField(validators=[validate_title], max_length=64)
     ects = models.IntegerField(validators=[MaxLengthValidator(600), MinValueValidator(1)])
     description = models.TextField()
     university = models.ForeignKey(University, on_delete=models.CASCADE)
 
-    def __init__(self, title, ects, description, university, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __str__(self):
+        return "{} [{}] {}".format(self.title, self.ects, self.description)
 
+    @staticmethod
+    def create(title, ects, description, university):
         if not isinstance(title, str) or re.match(r'^\s*$', title) or \
                 not isinstance(ects, int) or not (0 < ects < 601) or \
                 not isinstance(description, str) or \
                 not isinstance(university, University):
             raise ValueError
+        degree = Degree()
+        degree.title = title
+        degree.ects = ects
+        degree.description = description
+        degree.university = university
 
-        self.title = title
-        self.ects = ects
-        self.description = description
-        self.university = university
-
-    def __str__(self):
-        return "{} [{}] {}".format(self.title, self.ects, self.description)
+        return degree
--- univoting/tests/test_degree.py
+++ univoting/tests/test_degree.py
@@ -14,32 +14,60 @@ class TestDegree(TestCase):
     def tearDown(self):
         management.call_command('flush', verbosity=0, interactive=False)
 
+#
+########################################################################################################################
+    '''
+        Tests for validators:
+        Validators can not be tested with sqlite db
+    '''
+    '''
+    @parameterized.expand([[None], [123], [.265], [''], [' '], ['  '], [{}], [[]]])
+    def test_degree_name(self, degree_title):
+        degree = Degree()
+        with self.assertRaises(ValueError):
+            degree.title = degree_title
+
+    @parameterized.expand([[None], [0], [-1], [601], [""], [" "], [{}], [[]]])
+    def test_degree_ects(self, ects):
+        degree = Degree()
+        with self.assertRaises(ValueError):
+            degree.ects = ects
+    '''
+
+########################################################################################################################
+#
+
+#
+########################################################################################################################
+
     @parameterized.expand([[None], [123], [.265], [''], [' '], ['  '], [{}], [[]]])
     def test_degree_name(self, degree_name):
         with self.assertRaises(ValueError):
-            Degree(title=degree_name, ects=240, description="This is a description.", university=self.uni)
+            Degree.create(title=degree_name, ects=240, description="This is a description.", university=self.uni)
 
     @parameterized.expand([[None], [0], [-1], [601], [""], [" "], [{}], [[]]])
     def test_degree_ects(self, ects):
         with self.assertRaises(ValueError):
-            Degree(title='Exemple Title', ects=ects, description="This is a description.", university=self.uni)
+            Degree.create(title='Exemple Title', ects=ects, description="This is a description.", university=self.uni)
 
     @parameterized.expand([[None], [123], [.265], [{}], [[]]])
     def test_degree_description(self, description):
         with self.assertRaises(ValueError):
-            Degree(title='Exemple Title', ects=240, description=description, university=self.uni)
+            Degree.create(title='Exemple Title', ects=240, description=description, university=self.uni)
 
     @parameterized.expand([[None], [123], [.265], [""], [" "], [{}], [[]]])
     def test_degree_university(self, university):
         with self.assertRaises(ValueError):
-            Degree(title='Exemple Title', ects=240, description="This is a description.", university=university)
+            Degree.create(title='Exemple Title', ects=240, description="This is a description.", university=university)
 
+########################################################################################################################
+#
     @parameterized.expand([["Enginyeria Informatica", 240, "Aquest es el grau de enginyeria informatica."],
                            ["Enginyeria Electronica", 240, "Aquest es el grau de enginyeria electronica."],
                            ["Medicina", 300, "Aquest es el grau de medicina."],
                            ["Dret", 300, "Aquest es el grau de dret."]])
     def test_degree_values(self, title, ects, description):
-        degree = Degree(title=title, ects=ects, description=description, university=self.uni)
+        degree = Degree.create(title=title, ects=ects, description=description, university=self.uni)
         self.assertEqual(degree.title, title)
         self.assertEqual(degree.ects, ects)
         self.assertEqual(degree.description, description)

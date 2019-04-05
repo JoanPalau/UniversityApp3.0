8be364093a516bb581a03638d0f1ea7059ecdfbb
Toni Pacheco <toni.pafe@gmail.com>
2019-04-05 18:46:08 +0200

    Added the base for degrees list view.

--- univoting/views.py
+++ univoting/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
+from univoting.models.degree import Degree
 
-# Create your views here.
+
+class DegreeListView(ListView):
+    model = Degree
+    template_name = 'univoting/degree_list.html'
+
+    '''
+    slug_field = "university"
+    slug_url_kwarg = "university"
+
+    def get_queryset(self):
+        self.university = get_object_or_404(Degree, room=self.kwargs['university'])
+        return Degree.objects.filter(univesity=self.university)
+    '''
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Degrees'
+        return context

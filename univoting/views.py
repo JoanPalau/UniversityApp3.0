7f035cea636cdf50c7701d57fd01e6c053f22330
JoanPalau <joanp4l4u@gmail.com>
2019-04-09 23:25:32 +0200

    dev: vistes principals fetes, falta saber template names

--- univoting/views.py
+++ univoting/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from univoting.models.degree import Degree
+from univoting.models.subject import Subject
+from univoting.models.course import Course
+from univoting.models.degree import University
 
 
 def home(request):
@@ -27,4 +30,41 @@ class DegreeListView(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Degrees'
+        return context
+
+
+class DegreeDetailView(DetailView):
+    model = Degree
+    context_object_name = 'degree'
+    # template_name = '' No template yet
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = self.model.title
+
+        # Obtain list of subjects with their course for the Degree
+        subjects = Course.objects.filter(degree_id=self.model.pk)
+        context['subjects'] = subjects
+        return context
+
+
+class SubjectDetailView(DetailView):
+    model = Subject
+    context_object_name = 'subject'
+    # template_name = '' No template yet
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = self.model.name
+        return context
+
+
+class UniversityListView(ListView):
+    model = University
+    context_object_name = 'universities'
+    # template_name = '' No template yet
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Universities'
         return context

bf8fa2acda7427f7f0e476f495e571c6a217d5db
JoanPalau <joanp4l4u@gmail.com>
2019-04-13 18:53:51 +0200

    Afegits comentaris a les views

--- univoting/models/subject.py
+++ univoting/models/subject.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.urls import reverse
 from django.core.validators import MaxValueValidator, MinValueValidator
+from univoting.models.subject_review import SubjectReview
 
 
 class Subject(models.Model):
@@ -10,7 +11,7 @@ class Subject(models.Model):
     name = models.CharField(max_length=64, default="New Subject")
     ects = models.PositiveSmallIntegerField(validators=[MaxValueValidator(MAX_ECTS), MinValueValidator(MIN_ECTS)])
     description = models.TextField(max_length=250, default="No description added yet")
-    # review = models.ForeignKey()
+    review = models.ForeignKey(SubjectReview, on_delete=models.CASCADE)
 
     def __str__(self):
         return "{} {}".format(self.name, self.ects)

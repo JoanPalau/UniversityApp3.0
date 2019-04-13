d8bf1a7821f29d1dca7036924e56229ffb81818c
Toni Pacheco <toni.pafe@gmail.com>
2019-04-13 21:01:06 +0200

    Setting up de database: added degrees.

--- univoting/management/commands/db_manager.py
+++ univoting/management/commands/db_manager.py
@@ -1,6 +1,7 @@
 import os
 from django.core.management.base import BaseCommand
 from univoting.models.university import University
+from univoting.models.degree import Degree
 
 
 class Command(BaseCommand):
@@ -12,7 +13,10 @@ def make_database():
     path = os.getcwd()
     with open(path + '/data/head_description.data', 'r') as head_descr:
         with open(path + '/data/description.data', 'r') as description:
-            add_universities(path + '/data/university.data', head_descr.readline(), description.readline())
+            hdescr = head_descr.readline()
+            descr = description.readline()
+            add_universities(path + '/data/university.data', hdescr, descr)
+            add_degrees(path + '/data/degree.data', hdescr, descr)
 
 
 def add_universities(filename, head_description, description):
@@ -24,6 +28,22 @@ def add_universities(filename, head_description, description):
 
 def add_university(params, head_description, description):
     new_description = head_description.format(params[0])
-    new_description = new_description + description
-    uni = University(name=params[0], description=new_description, picture=params[1])
+    new_description = new_description + ' ' + description
+    uni = University(name=params[0], description=new_description, picture=params[1].rstrip())
     uni.save()
+
+
+def add_degrees(filename, head_description, description):
+    with open(filename, 'r') as f:
+        for line in f.readlines():
+            params = line.split('|')
+            universities = University.objects.all()
+            for uni in universities:
+                add_degree(params, head_description, description, uni)
+
+
+def add_degree(params, head_description, description, uni):
+    new_description = head_description.format(params[0])
+    new_description = new_description + ' ' + description
+    degree = Degree(title=params[0], ects=params[1], description=new_description, university=uni)
+    degree.save()

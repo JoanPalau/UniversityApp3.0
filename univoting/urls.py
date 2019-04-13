9f3ba4b61190db2f2ab3fe3cc5f025af02150bc6
Toni Pacheco <toni.pafe@gmail.com>
2019-04-13 18:43:58 +0200

    Added url for subjects.

--- univoting/urls.py
+++ univoting/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
+from .views import home, UniversityListView, UniversityDetailView, DegreeDetailView, SubjectDetailView
 from .views import universities_mock, university_mock, degree_mock, subject_mock
-from .views import home, UniversityListView, UniversityDetailView, DegreeDetailView
 
 
 urlpatterns = [
@@ -8,9 +8,9 @@ urlpatterns = [
     path('universities/', UniversityListView.as_view(), name='universities'),
     path('universities/<int:pk>/', UniversityDetailView.as_view(), name='university'),
     path('degree/<int:pk>/', DegreeDetailView.as_view(), name='degree'),
-
-    path('subject/', subject_mock, name='subject'),
+    path('subject/<int:pk>/', SubjectDetailView.as_view(), name='subject'),
     # path('universities/', universities_mock, name='universities'),
     # path('university/', university_mock, name='university'),
     # path('degree/', degree_mock, name='degree'),
+    # path('subject/', subject_mock, name='subject'),
 ]

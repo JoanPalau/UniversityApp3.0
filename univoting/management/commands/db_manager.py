da4575518a29e7c3952beabb1496eb30f6669854
Toni Pacheco <toni.pafe@gmail.com>
2019-04-14 12:51:20 +0200

    Setting up de database: added subjects, subject reviews and courses.

--- univoting/management/commands/db_manager.py
+++ univoting/management/commands/db_manager.py
@@ -1,7 +1,11 @@
 import os
+import random
 from django.core.management.base import BaseCommand
 from univoting.models.university import University
 from univoting.models.degree import Degree
+from univoting.models.subject import Subject
+from univoting.models.subject_review import SubjectReview
+from univoting.models.course import Course
 
 
 class Command(BaseCommand):
@@ -17,6 +21,7 @@ def make_database():
             descr = description.readline()
             add_universities(path + '/data/university.data', hdescr, descr)
             add_degrees(path + '/data/degree.data', hdescr, descr)
+            add_subjects(path + '/data/subjects.data', hdescr, descr)
 
 
 def add_universities(filename, head_description, description):
@@ -27,8 +32,7 @@ def add_universities(filename, head_description, description):
 
 
 def add_university(params, head_description, description):
-    new_description = head_description.format(params[0])
-    new_description = new_description + ' ' + description
+    new_description = get_description(head_description, description, params[0])
     uni = University(name=params[0], description=new_description, picture=params[1].rstrip())
     uni.save()
 
@@ -39,11 +43,57 @@ def add_degrees(filename, head_description, description):
             params = line.split('|')
             universities = University.objects.all()
             for uni in universities:
-                add_degree(params, head_description, description, uni)
+                if random.random() > 0.66:
+                    add_degree(params, head_description, description, uni)
 
 
 def add_degree(params, head_description, description, uni):
-    new_description = head_description.format(params[0])
-    new_description = new_description + ' ' + description
-    degree = Degree(title=params[0], ects=params[1], description=new_description, university=uni)
+    new_description = get_description(head_description, description, params[0])
+    degree = Degree(title=params[0], ects=params[1],
+                    description=new_description, university=uni)
     degree.save()
+
+
+def add_subjects(filename, head_description, description):
+    degrees = Degree.objects.all()
+    i_course = 0
+    with open(filename, 'r') as f:
+        for i_subject, line in enumerate(f.readlines()):
+            params = line.split('|')
+            if i_subject % 6 == 0:
+                i_course += 1
+            for degree in degrees:
+                new_subject = add_subject(params, i_course, i_subject+1,
+                                          head_description, description)
+                add_new_course(degree, new_subject, i_course)
+
+
+def add_subject(params, i_curse, i_subject, head_description, description):
+    new_description = get_description(head_description, description, params[0])
+    review = get_new_review()
+    review.save()
+    new_name = 'Subject-{}.{}'.format(i_curse, i_subject)
+    subject = Subject(name=new_name, ects=params[0],
+                      description=new_description, review=review)
+    subject.save()
+    return subject
+
+
+def add_new_course(degree, subject, course):
+    course = Course(degree_id=degree, subject_id=subject, course=course)
+    course.save()
+
+
+def get_new_review():
+    mark = random.random()*10
+    difficulty = random.random()*10
+    work_score = random.random()*2
+    work_volume = int(work_score + .5)
+    review = SubjectReview(mark=mark, difficulty=difficulty,
+                           work_score=work_score, workVolume=work_volume,
+                           amount=1)
+    return review
+
+
+def get_description(head_description, description, name):
+    return head_description.format(name) + description

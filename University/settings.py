356e36e261c7250915e6f08a8fda1052d54fa0e1
Toni Pacheco <toni.pafe@gmail.com>
2019-04-30 15:28:43 +0200

    Trying to solve one issue with crispy in the register page.

--- University/settings.py
+++ University/settings.py
@@ -31,10 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'crispy_forms',
     'univoting.apps.UnivotingConfig',
     'users.apps.UsersConfig',
     'markdown_deux',
-    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',

a13ce0be72ec0c29aa700bc5500f1c9bfe235a47
Toni Pacheco <toni.pafe@gmail.com>
2019-05-08 17:46:41 +0200

    BDD: Added features and steps for  Degrees list in universities details page.

---
+++ features/steps/view_university.py
@@ -0,0 +1,22 @@
+from behave import *
+
+use_step_matcher("parse")
+
+
+@when('I view the details for university "{university_name}"')
+def step_impl(context, university_name):
+    from univoting.models import University
+    university = University.objects.get(name=university_name)
+    context.browser.visit(context.get_url(university))
+
+
+@Then('I\'m viewing a University degrees list containing')
+def step_impl(context):
+    degrees_links = context.browser.find_by_css('div#degrees-group ul li a')
+    for i, row in enumerate(context.table):
+        assert row['name'] == degrees_links[i].text
+
+
+@Then('The list contains {count:n} degrees')
+def stem_impl(context, count):
+    assert count == len(context.browser.find_by_css('div#degrees-group ul li a'))

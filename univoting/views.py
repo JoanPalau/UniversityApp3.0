ae4eb97cabf5aa10f088f107deb258ea6f0f3c4d
JoanPalau <joanp4l4u@gmail.com>
2019-05-10 16:21:29 +0200

    minnor fix

--- univoting/views.py
+++ univoting/views.py
@@ -99,7 +99,7 @@ class DegreeCreateView(LoginRequiredMixin, CreateView):
     template_name = 'univoting/university-register.html'
 
     def form_valid(self, form):
-        form.instance.university = get_object_or_404(University, self.kwargs['pk'])
+        form.instance.university = get_object_or_404(University, pk=self.kwargs['pk'])
         form.instance.created_by = self.request.user
         return super().form_valid(form)
 

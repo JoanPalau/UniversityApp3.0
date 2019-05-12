4e1d0e3ac8400b01ebad25a632f0e12f3625cae3
JoanPalau <joanp4l4u@gmail.com>
2019-05-12 12:44:21 +0200

    Arreglat problema a la creacio de course

--- univoting/signals.py
+++ univoting/signals.py
@@ -2,12 +2,14 @@ from django.db.models.signals import post_save, pre_delete
 from django.dispatch import receiver
 from univoting.models.course import Course
 from univoting.models.subject import Subject
+from univoting.models.degree import Degree
 
 
 @receiver(post_save, sender=Subject)
 def create_course(sender, instance, created, **kwargs):
     if created:
-        Course.objects.create(subject_id=instance, degree_id=instance.get_degree(), course=instance.get_course())
+        Course.objects.create(subject_id=instance, degree_id= Degree.objects.get(pk=instance.get_degree()),
+                              course=instance.get_course())
 
 
 @receiver(pre_delete, sender=Subject)
